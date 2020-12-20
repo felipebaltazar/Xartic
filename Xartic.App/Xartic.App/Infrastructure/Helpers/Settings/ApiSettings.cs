5751746002aa827b0cefddb7c21b429fe121f975
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-12-20 01:22:34 -0300

    ✨ Using Settings.Json

---
+++ Xartic.App/Xartic.App/Infrastructure/Helpers/Settings/ApiSettings.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Xamarin.Forms.Internals;
+
+namespace Xartic.App.Infrastructure.Helpers.Settings
+{
+    [Preserve(AllMembers = true)]
+    [JsonObject("api")]
+    public sealed class ApiSettings
+    {
+        [JsonProperty("baseUrl")]
+        public string BaseUrl { get; set; }
+    }
+}
---
+++ Xartic.App/Xartic.App/Infrastructure/Helpers/Settings/AppCenterSettings.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Xamarin.Forms.Internals;
+
+namespace Xartic.App.Infrastructure.Helpers
+{
+    [JsonObject("appcenter")]
+    public sealed class AppCenterSettings
+    {
+        [Preserve]
+        public AppCenterSettings()
+        {
+        }
+
+        [JsonProperty("android")]
+        public string AndroidKey { get; set; }
+
+        [JsonProperty("ios")]
+        public string IosKey { get; set; }
+    }
+}
