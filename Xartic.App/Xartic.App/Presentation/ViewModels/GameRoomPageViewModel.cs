fd2e05120cd859b9163d7209d2567f12069b8393
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-12-20 01:26:24 -0300

    ✨ Stop xartic connection when app goes background or page popped

--- Xartic.App/Xartic.App/Presentation/ViewModels/GameRoomPageViewModel.cs
+++ Xartic.App/Xartic.App/Presentation/ViewModels/GameRoomPageViewModel.cs
@@ -1,22 +1,26 @@
-﻿using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Xamarin.Forms;
-using Xartic.App.Abstractions;
+﻿using Xartic.App.Abstractions;
 using Xartic.App.Abstractions.Navigation;
 using Xartic.App.Domain.Models;
 using Xartic.App.Infrastructure.Extensions;
 using Xartic.App.Infrastructure.Helpers;
-using Xartic.Core;
+using Xamarin.Forms.Internals;
 using XFColor = Xamarin.Forms.Color;
+using System.Threading;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Xamarin.Forms;
+using Xartic.Core;
+using Microsoft.Extensions.Logging;
+using System.Windows.Input;
 
 namespace Xartic.App.Presentation.ViewModels
 {
-    public sealed class GameRoomPageViewModel : BaseViewModel, INavigatedAware
+    [Preserve(AllMembers = true)]
+    public sealed class GameRoomPageViewModel : BaseViewModel, INavigatedAware, IDisposable, IApplicationLifeCycleAware
     {
+        #region Fields
+
         private readonly IXarticService _xarticService;
         private readonly ILogger _logger;
 
@@ -28,13 +32,19 @@ namespace Xartic.App.Presentation.ViewModels
         private string message;
         private bool hasWinner;
 
+        #endregion
+
+        #region Properties
+
         public ObservableRangeCollection<Player> Players { get; }
+
         public ObservableRangeCollection<IRenderer> Renderers { get; }
+
         public ObservableRangeCollection<ChatMessage> Messages { get; }
 
         public ICommand MessageSendCommand { get; }
 
-        public string Username 
+        public string Username
         {
             get => username;
             set => SetProperty(ref username, value);
@@ -58,6 +68,10 @@ namespace Xartic.App.Presentation.ViewModels
             set => SetProperty(ref message, value);
         }
 
+        #endregion
+
+        #region Constructors
+
         public GameRoomPageViewModel(
             IXarticService xarticService,
             ILogger logger)
@@ -70,6 +84,10 @@ namespace Xartic.App.Presentation.ViewModels
             Messages = new ObservableRangeCollection<ChatMessage>();
             MessageSendCommand = new Command(MessageSendCommandExecute);
         }
+
+        #endregion
+
+        #region INavigatedAware
 
         public async Task OnNavigatedAsync(IDictionary<string, StringValues> parameters)
         {
@@ -90,15 +108,66 @@ namespace Xartic.App.Presentation.ViewModels
             }
         }
 
+        #endregion
+
+        #region IApplicationLifeCycleAware
+
+        public async Task OnSleepAsync(CancellationToken token)
+        {
+            try
+            {
+                await ExecuteBusyTask(_xarticService.CloseConnectionAsync, token).ConfigureAwait(false);
+            }
+            catch (TaskCanceledException)
+            {
+                _logger.LogInformation($"{nameof(OnSleepAsync)} task canceled");
+            }
+        }
+
+        public async Task OnResumeAsync(CancellationToken token)
+        {
+            try
+            {
+                await ExecuteBusyTask(OpenSignalRConnectionAsync, token).ConfigureAwait(false);
+            }
+            catch (TaskCanceledException)
+            {
+                _logger.LogInformation($"{nameof(OnResumeAsync)} task canceled");
+            }
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        public void Dispose()
+        {
+            DisposeAsync().SafeFireAndForget();
+
+            cancellationTokenSource?.Cancel();
+            cancellationTokenSource?.Dispose();
+            cancellationTokenSource = null;
+        }
+
+        private Task DisposeAsync() =>
+            _xarticService.CloseConnectionAsync();
+
+        #endregion
+
+        #region Private Methods
+
         private async Task OpenSignalRConnectionAsync()
         {
             await _xarticService.OpenConnectionAsync(Username, RoomName).ConfigureAwait(false);
 
-            _xarticService.SubscribeFor<RoomStatus>("OnRoomStatusChanged", OnRoomStatusChanged);
-            _xarticService.SubscribeFor<string>("ResponseResult", OnResponseResultReceived);
-            _xarticService.SubscribeFor<string, string>("Message", OnChatMessageReceived);
-            _xarticService.SubscribeFor<DrawCommand>("Draw", OnDrawCommandReceived);
-            _xarticService.SubscribeFor("Clear", OnClearCanvasReceived);
+            if (_xarticService.IsConnected)
+            {
+                _xarticService.SubscribeFor<RoomStatus>("OnRoomStatusChanged", OnRoomStatusChanged);
+                _xarticService.SubscribeFor<string>("ResponseResult", OnResponseResultReceived);
+                _xarticService.SubscribeFor<string, string>("Message", OnChatMessageReceived);
+                _xarticService.SubscribeFor<DrawCommand>("Draw", OnDrawCommandReceived);
+                _xarticService.SubscribeFor("Clear", OnClearCanvasReceived);
+            }
         }
 
         private void MessageSendCommandExecute()
@@ -136,9 +205,15 @@ namespace Xartic.App.Presentation.ViewModels
             Players.AddRange(status.Players);
 
             HasWinner = status.HasWinner();
+
+            if (!status.CurrentDraw.IsNullOrEmpty())
+            {
+                foreach (var command in status.CurrentDraw)
+                    OnDrawCommandReceived(command);
+            }
         }
 
-        private void OnClearCanvasReceived(string username)
+        private void OnClearCanvasReceived()
         {
             Renderers.Clear();
             lastCommand = null;
@@ -146,10 +221,12 @@ namespace Xartic.App.Presentation.ViewModels
 
         private void OnDrawCommandReceived(DrawCommand command)
         {
-            if(command.IsMouseDown && lastCommand != null)
+            if (command.IsMouseDown && lastCommand != null)
                 Renderers.Add(command.ToRenderer(lastCommand));
 
             lastCommand = command;
         }
+
+        #endregion
     }
 }
